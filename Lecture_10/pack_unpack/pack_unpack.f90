c3e6644d1cd866bd5449439d6b429b78523492ef
Andrii V. Sukhorukov <andrii@iac.es>
2021-11-03 17:59:28 +0000

    Correct pack_unpack.f90
    Use comm variable for the world communicator.

--- Lecture_10/pack_unpack/pack_unpack.f90
+++ Lecture_10/pack_unpack/pack_unpack.f90
@@ -2,6 +2,7 @@ program pack_unpack
     use mpi_f08
     implicit none
     integer :: n_ranks, my_rank, first, last
+    type(MPI_Comm) :: comm
     type(MPI_Status) :: status
     integer          :: i = 0
     real             :: x = 0.0
@@ -12,8 +13,9 @@ program pack_unpack
     integer :: position
 
     call MPI_Init()
-    call MPI_Comm_size(MPI_COMM_WORLD, n_ranks)
-    call MPI_Comm_rank(MPI_COMM_WORLD, my_rank)
+    comm = MPI_COMM_WORLD
+    call MPI_Comm_size(comm, n_ranks)
+    call MPI_Comm_rank(comm, my_rank)
 
     first = 0
     last  = n_ranks - 1
@@ -23,17 +25,17 @@ program pack_unpack
         x    = -1.0
         name = 'test'
         position = 0
-        call MPI_Pack(i,    1,         MPI_INTEGER,   my_buffer, BUF_LEN, position, MPI_COMM_WORLD)
-        call MPI_Pack(x,    1,         MPI_REAL,      my_buffer, BUF_LEN, position, MPI_COMM_WORLD)
-        call MPI_Pack(name, len(name), MPI_CHARACTER, my_buffer, BUF_LEN, position, MPI_COMM_WORLD)
-        call MPI_Send(my_buffer, position, MPI_PACKED, last, 0, MPI_COMM_WORLD)
+        call MPI_Pack(i,    1,         MPI_INTEGER,   my_buffer, BUF_LEN, position, comm)
+        call MPI_Pack(x,    1,         MPI_REAL,      my_buffer, BUF_LEN, position, comm)
+        call MPI_Pack(name, len(name), MPI_CHARACTER, my_buffer, BUF_LEN, position, comm)
+        call MPI_Send(my_buffer, position, MPI_PACKED, last, 0, comm)
     else if (my_rank == last) then
         print "(2(a, i0), a, f5.1, 3a)", "Rank ", my_rank, ": i=", i, ", x=", x, ", name='", name, "'."
-        call MPI_Recv(my_buffer, BUF_LEN, MPI_PACKED, first, 0, MPI_COMM_WORLD, status)
+        call MPI_Recv(my_buffer, BUF_LEN, MPI_PACKED, first, 0, comm, status)
         position = 0
-        call MPI_Unpack(my_buffer, BUF_LEN, position, i,    1,         MPI_INTEGER,   MPI_COMM_WORLD)
-        call MPI_Unpack(my_buffer, BUF_LEN, position, x,    1,         MPI_REAL,      MPI_COMM_WORLD)
-        call MPI_Unpack(my_buffer, BUF_LEN, position, name, len(name), MPI_CHARACTER, MPI_COMM_WORLD)
+        call MPI_Unpack(my_buffer, BUF_LEN, position, i,    1,         MPI_INTEGER,   comm)
+        call MPI_Unpack(my_buffer, BUF_LEN, position, x,    1,         MPI_REAL,      comm)
+        call MPI_Unpack(my_buffer, BUF_LEN, position, name, len(name), MPI_CHARACTER, comm)
         print "(2(a, i0), a, f5.1, 3a)", "Rank ", my_rank, ": i=", i, ", x=", x, ", name='", name, "'."
     end if
 
